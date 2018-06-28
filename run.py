3b984191dc721526144bb56298f7af847cb8cdf3
Anne Marie Johnston <annemarie.johnston@outlook.com>
2018-06-28 13:08:16 +0000

    added chat.html //

--- run.py
+++ run.py
@@ -12,9 +12,10 @@ def add_messages(username, message):
     messages_dict = {"timestamp": now, "from": username, "message": message}
     messages.append(messages_dict)
 
+
 def get_all_messages():
-    """Get al of the messages and separate them by a `br` now"""
-    return "<br>".join(messages)
+    """Get al of the messages and separate them by a `br`"""
+    return messages
 
 
 @app.route('/', methods=["GET", "POST"])
@@ -22,22 +23,23 @@ def index():
     """Main page with instructions"""
     if request.method == "POST":
         with open("data/users.txt", "a") as user_list:
-            user_list.write(request.form["username"])
+            user_list.writelines(request.form["username"] + "\n")
         return redirect(request.form["username"])
     return render_template("index.html")
 
 
 @app.route('/<username>')
 def user(username):
-    """Display chat messages   TODO: KEEP `get_all_messages` """
-    return "<h1>Welcome, {0}</h1> {1}".format(username, get_all_messages())
+    """Display chat messages"""
+    messages = get_all_messages()
+    return render_template("chat.html",
+                            username=username, chat_messages=messages)
 
 
 @app.route('/<username>/<message>')
 def send_message(username, message):
     """Create a new message and redirect back to the chat page"""
-    add_messages(username, message)
+    add_messages(username, get_all_messages())
     return redirect(username)
-
 
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
