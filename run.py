7d8502a1f2460fd60213a8eca4f2a256f0fe8b40
Anne Marie Johnston <annemarie.johnston@outlook.com>
2018-07-01 11:01:24 +0000

    refactoring code, added .title() to username in H1

--- run.py
+++ run.py
@@ -1,46 +1,55 @@
 import os
 from datetime import datetime
-from flask import Flask, render_template, request, redirect
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
 
+def write_to_file(filename, data):
+    """Handle the process of writing data to a file"""
+    with open(filename, "a") as file:
+        file.writelines(data)
+
+
 def add_messages(username, message):
-    """Add messages to the `messages` list"""
-    now = datetime.now().strftime("%H:%M:%S")
-    messages_dict = {"timestamp": now, "from": username, "message": message}
-#Write Chat messages to the chat.txt file
-    with open ("data/messages.txt", "a") as chat_list:
-        chat_list.writelines("({0}) {1} - {2}\n".format(
-            messages_dict["timestamp"],
-            messages_dict["from"].title(),
-            messages_dict["message"] ))
+    """Add messages to the `messages` text file"""
+    write_to_file("data/messages.txt", "({0}) {1} - {2}\n".format(
+            datetime.now().strftime("%H:%M:%S"),
+            username.title(),
+            message))
 
 
 def get_all_messages():
-    """Get al of the messages and write them into messages.txt`"""
+    """Get all of the messages and separate them by a `br`"""
     messages = []
-    with open ("data/messages.txt", "r") as chat_messages:
+    with open("data/messages.txt", "r") as chat_messages:
         messages = chat_messages.readlines()
-        return messages
+    return messages
 
 
 @app.route('/', methods=["GET", "POST"])
 def index():
     """Main page with instructions"""
+    
+    # Handle POST request
     if request.method == "POST":
-        with open("data/users.txt", "a") as user_list:
-            user_list.writelines(request.form["username"] + "\n")
+        write_to_file("data/users.txt", request.form["username"] + "\n")
         return redirect(request.form["username"])
     return render_template("index.html")
 
 
-@app.route('/<username>')
+@app.route('/<username>', methods=["GET", "POST"])
 def user(username):
     """Display chat messages"""
+    
+    if request.method == "POST":
+        add_messages(username, request.form["message"] + "\n")
+    
     messages = get_all_messages()
     return render_template("chat.html",
-                            username=username, chat_messages=messages)
+                            username=username.title(), chat_messages=messages)
+# on Line 50 I added a .title() to display the User name in capital letters in the Welcome H1
+
 
 @app.route('/<username>/<message>')
 def send_message(username, message):
